57be696c7fb8d123b2c830f87968095b20d3c62f
Alejandro Rodríguez Arias <alejandro.rodriguez.arias@udc.es>
2023-09-21 11:11:13 +0200

    vitoria

---
+++ Assets/Examples/Vitoria/Scripts/EnvironmentGeneratorVitoria.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TriangleNet.Topology;
+using SEGAR;
+using Newtonsoft.Json.Linq;
+using System;
+using TriangleNet.Geometry;
+
+public class EnvironmentGeneratorVitoria : EnvironmentGenerator
+{
+
+    /// <summary>
+    /// Create the agents using the sociodemographic data. These agents are empty. It is only a demo for the population of the environment.ion
+    /// </summary>
+    protected override void CreateSimulatedAgents()
+    {
+        censusDataJson = JsonUtility.FromJson<CensusData>(censusData.text);
+        int currentNumberAgents = 0;
+        foreach (CensusDataSec censusDataSec in censusDataJson.censusData)
+        {
+            ICollection<Triangle> triangles = polygons[secRef[censusDataSec.secRef]];
+            Triangle[] triangleArray = new Triangle[triangles.Count];
+            triangles.CopyTo(triangleArray, 0);
+            int maleOver65 = censusDataSec.t7_3 / 10;
+            CreateMaleOver65(maleOver65, currentNumberAgents, censusDataSec.secRef, triangleArray);
+            int maleUnder65 = censusDataSec.t7_2 / 10;
+            currentNumberAgents = currentNumberAgents + maleOver65;
+            CreateMaleUnder65(maleUnder65, currentNumberAgents, censusDataSec.secRef, triangleArray);
+            int femaleOver65 = censusDataSec.t7_6 / 10;
+            currentNumberAgents = currentNumberAgents + maleUnder65;
+            CreateFemaleOver65(femaleOver65, currentNumberAgents, censusDataSec.secRef, triangleArray);
+            int femaleUnder65 = censusDataSec.t7_5 / 10;
+            currentNumberAgents = currentNumberAgents + femaleOver65;
+            CreateFemaleUnder65(femaleUnder65, currentNumberAgents, censusDataSec.secRef, triangleArray);
+            currentNumberAgents = currentNumberAgents + femaleUnder65;
+        }
+    }
+
+    public void CreateMaleOver65(int numberNewAgents, int currentNumberAgents, string cusec, Triangle[] triangles)
+    {
+
+        for (int i = 1; i <= numberNewAgents; i++)
+        {
+            GameObject agent = Instantiate(citizenPrefab);
+            agent.transform.position = GenerateRandomPointWithinTriangle(ChooseRandomTriangle(triangles));
+            Citizen citizenAgent = agent.GetComponent<Citizen>();
+            agent.name = "Citizen " + (i + currentNumberAgents);
+            citizenAgent.gender = 0;
+            citizenAgent.age = 65 + UnityEngine.Random.Range(1, 36);
+        }
+    }
+
+    public void CreateFemaleOver65(int numberNewAgents, int currentNumberAgents, string cusec, Triangle[] triangles)
+    {
+
+        for (int i = 1; i <= numberNewAgents; i++)
+        {
+            GameObject agent = Instantiate(citizenPrefab);
+            agent.transform.position = GenerateRandomPointWithinTriangle(ChooseRandomTriangle(triangles));
+            Citizen citizenAgent = agent.GetComponent<Citizen>();
+            agent.name = "Citizen " + (i + currentNumberAgents);
+            citizenAgent.gender = 1;
+            citizenAgent.age = 65 + UnityEngine.Random.Range(1, 36);
+        }
+    }
+
+    public void CreateMaleUnder65(int numberNewAgents, int currentNumberAgents, string cusec, Triangle[] triangles)
+    {
+
+        for (int i = 1; i <= numberNewAgents; i++)
+        {
+            GameObject agent = Instantiate(citizenPrefab);
+            agent.transform.position = GenerateRandomPointWithinTriangle(ChooseRandomTriangle(triangles));
+            Citizen citizenAgent = agent.GetComponent<Citizen>();
+            agent.name = "Citizen " + (i + currentNumberAgents);
+            citizenAgent.gender = 0;
+            citizenAgent.age = 18 + UnityEngine.Random.Range(0, 47);
+        }
+    }
+
+    public void CreateFemaleUnder65(int numberNewAgents, int currentNumberAgents, string cusec, Triangle[] triangles)
+    {
+
+        for (int i = 1; i <= numberNewAgents; i++)
+        {
+            GameObject agent = Instantiate(citizenPrefab);
+            agent.transform.position = GenerateRandomPointWithinTriangle(ChooseRandomTriangle(triangles));
+            Citizen citizenAgent = agent.GetComponent<Citizen>();
+            agent.name = "Citizen " + (i + currentNumberAgents);
+            citizenAgent.gender = 1;
+            citizenAgent.age = 18 + UnityEngine.Random.Range(0, 47);
+        }
+    }
+}
